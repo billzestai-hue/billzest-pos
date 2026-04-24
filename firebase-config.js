5996e6be0d733153cc35b05804e4774ade54b6df
billzestai-hue <billzestai@gmail.com>
2026-04-24 05:19:02 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,6 @@
-// PASTE YOUR FIREBASE CONFIG HERE:
-// Go to console.firebase.google.com -> Project Settings -> Web App to get this.
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-// Import Firebase (v10 Modular SDK) from CDN
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
