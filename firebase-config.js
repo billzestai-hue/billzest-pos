d17868558e3d016ff5bc96f13ab89ca01dc378da
billzestai-hue <billzestai@gmail.com>
2026-04-24 05:23:40 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,28 @@
+// Firebase Configuration - REAL VALUES
+const firebaseConfig = {
+  apiKey: "AIzaSyCN0akvFXOHM6WPHv9ve9noqqmauO3TJRY",
+  authDomain: "billzest-pos.firebaseapp.com",
+  projectId: "billzest-pos",
+  storageBucket: "billzest-pos.firebasestorage.app",
+  messagingSenderId: "946546876429",
+  appId: "1:946546876429:web:9b4a7aa3cecbf3aea83544",
+  measurementId: "G-69WEHH9X5D"
+};
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, sendPasswordResetEmail, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-export { auth, provider, signInWithEmailAndPassword, signInWithPopup, onAuthStateChanged, signOut };
+export { 
+  auth, 
+  googleProvider, 
+  signInWithEmailAndPassword, 
+  signInWithPopup, 
+  onAuthStateChanged, 
+  signOut,
+  sendPasswordResetEmail,
+  createUserWithEmailAndPassword
+};
